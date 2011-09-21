09b57ddef2c04fad2545eb1837b43e9fc96292a1
Joost Schuur <jschuur@jschuur.com>
2011-09-21 04:50:52 -0700

    Set correct communityvisibilitystate.

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
       user.steam_real_name = auth["user_info"]["name"]
       user.steam_nickname = auth["user_info"]["nickname"]
 
-      user.steam_privacy_state = ["private", "friendsonly", "public"][player["communityvisibilitystate"].to_i]
+      user.steam_privacy_state = ["private", "friendsonly", "public"][player["communityvisibilitystate"].to_i - 1]
 
       user.steam_base_url = player["profileurl"]
       user.steam_custom_url = user.steam_base_url.split('/')[4] if user.steam_base_url.length > 0
